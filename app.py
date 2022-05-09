47cdeac05881c6b3e3943cb8430fbe285f278424
AlexLiCa <alexlin.carre@gmail.com>
2022-05-09 17:47:05 -0500

    PORT

--- app.py
+++ app.py
@@ -276,9 +276,9 @@ def deleteh2(id):
         return redirect(request.referrer)
 
 
-port = int(os.environ.get('PORT', 8080))
+PORT = int(os.environ.get('PORT', 8080))
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=port, threaded=True)
+    app.run(host='0.0.0.0', port=PORT, threaded=True)
 
 
 
